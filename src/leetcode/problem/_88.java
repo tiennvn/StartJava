e1b7aa24d19c56fc8c38fb5cf457b38aacb0d0d5
tienn <tienn.tmtco@gmail.com>
2023-10-07 14:11:04 +0700

    problem 88

--- src/leetcode/problem/_88.java
+++ src/leetcode/problem/_88.java
@@ -43,12 +43,13 @@ package leetcode.problem;
 
 public class _88 {
     public static void main(String[] args) {
-        // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-        var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
-        var m = 3;
-        var nums2 = new int[] { 2, 5, 6 };
-        var n = 3;
+        var nums1 = new int[] { -1,3,0,0,0,0,0 };
+        var m = 2;
+        var nums2 = new int[] { 0,0,1,2,3 };
+        var n = 5;
+
         Solution.merge(nums1, m, nums2, n);
+
         for (int i : nums1) {
             System.out.print(i + "-");
         }
@@ -56,7 +57,54 @@ public class _88 {
 
     class Solution {
         public static void merge(int[] nums1, int m, int[] nums2, int n) {
-            
+            // Precheck
+            if (n == 0)
+            {
+                return;
+            }
+            if (m == 0)
+            {
+                for (int i = 0; i < nums1.length; i++)
+                {
+                    nums1[i] = nums2[i];
+                }
+                return;
+            }
+
+            // Process
+            var nums1Index = m - 1;
+            var nums2Index = n - 1;
+            var resultIndex = m + n - 1;
+
+            for (int i = resultIndex; i >= 0; i--) {
+                
+                if (nums1Index < 0)
+                {
+                    nums1[i] = nums2[nums2Index];
+                    nums2Index--;
+                    continue;
+                }
+                if (nums2Index < 0)
+                {
+                    nums1[i] = nums1[nums1Index];
+                    nums1Index--;
+                    continue;
+                }
+                var num1 = nums1[nums1Index];
+                var num2 = nums2[nums2Index];
+                if (num1 >= num2)
+                {
+                    nums1[i] = num1;
+                    nums1Index--;
+                    continue;
+                }
+                else
+                {
+                    nums1[i] = num2;
+                    nums2Index--;
+                    continue;
+                }
+            }
         }
     }
 }
